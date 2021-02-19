059e7dfdfde6ca03d1a3133d58a58a627a285cd6
Josh Mellicker <josh@platformpurple.com>
2021-02-18 20:33:48 -0800

    add button

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,19 @@
 <template>
   <q-page class="q-pa-lg">
-    <div class="row full-width full-height q-col-gutter-md">
-      <div class="col">
-        <q-input
-          v-model="text"
-          class="full-width full-height"
-          filled
-          type="textarea"
-        />
-      </div>
-      <div class="col">
-        scroll area here
-      </div>
+    <div class="row full-width q-col-gutter-md justify-center">
+      here is the text we want to scroll
+      <q-input
+        v-model="text"
+        class="full-width full-height"
+        filled
+        type="textarea"
+      />
+    </div>
+    <div class="row q-mt-xl justify-center">
+      <q-btn color="primary" size="lg" label="start scrolling" @click="startScrolling" />
+    </div>
+    <div class="row q-mt-xl justify-center">
+      show scrolling text here
     </div>
   </q-page>
 </template>
@@ -24,6 +26,11 @@ export default {
   data () {
     return {
       text
+    }
+  },
+  methods: {
+    startScrolling () {
+
     }
   }
 }
