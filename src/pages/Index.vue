5d44ebf1a100b4474cf11523a34381edde6c77fd
Josh Mellicker <josh@platformpurple.com>
2021-02-18 20:24:38 -0800

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="q-pa-lg">
+    <div class="row full-width full-height q-col-gutter-md">
+      <div class="col">
+        <q-input
+          v-model="text"
+          class="full-width full-height"
+          filled
+          type="textarea"
+        />
+      </div>
+      <div class="col">
+        scroll area here
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import text from '../assets/text.js'
+
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      text
+    }
+  }
+}
+</script>
