82236c79563e00fbfc606f11e4cc688d09c4442b
leonzerphio <leonzephiro@gmail.com>
2021-02-20 13:03:25 -0300

    - upgraded quasar version

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,10 +10,29 @@
       />
     </div>
     <div class="row q-mt-xl justify-center">
-      <q-btn color="primary" size="lg" label="start scrolling" @click="startScrolling" />
+      <q-btn
+        color="primary"
+        size="lg"
+        label="start scrolling"
+        @click="startScrolling"
+      />
     </div>
-    <div class="row q-mt-xl justify-center">
-      show scrolling text here
+    <div class="row q-mt-xl justify-center text-white">
+      <q-scroll-area
+        dark
+        class="bg-dark text-white rounded-borders"
+        style="height: 200px; max-width: 300px;"
+      >
+        <div
+          v-for="n in 100"
+          :key="n"
+          class="q-py-sm q-px-md"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua.
+        </div>
+      </q-scroll-area>
     </div>
   </q-page>
 </template>
@@ -25,13 +44,33 @@ export default {
   name: 'PageIndex',
   data () {
     return {
-      text
+      text,
+      scrolling: null
     }
   },
   methods: {
+    getYpos () {
+      var ypos = this.$refs.text.offsetTop;
+      var thisNode = this.$refs.text;
+      while (thisNode.offsetParent && (thisNode.offsetParent != document.body)) {
+        thisNode = thisNode.offsetParent;
+        ypos += 0.5;
+      }
+      return ypos;
+    },
     startScrolling () {
-
-    }
+      if (this.scrolling) {
+        window.clearTimeout(this.scrolling);
+        this.scrolling = null;
+      } else {
+        this.doScroll();
+      }
+    },
+    doScroll () {
+      var y = parseInt(this.getYpos());
+      this.styleTopNumber = y
+      this.scrolling = window.setTimeout(this.doScroll, 200)
+    },
   }
 }
 </script>
